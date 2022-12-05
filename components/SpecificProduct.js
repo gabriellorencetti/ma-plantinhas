feb842dfa420b9f045f8f733677b3c626c91abd0
gabriellorencetti <lorencetti.gabriel@gmail.com>
2022-12-05 19:50:34 -0300

    Fix key prop and alt on images

--- components/SpecificProduct.js
+++ components/SpecificProduct.js
@@ -1,97 +1,93 @@
-import product_list from "../pages/api/product_list"
+import product_list from "../pages/api/product_list";
 import Image from "next/image";
 import styles from "../styles/Product.module.css";
-import React from "react"
-import Rating from '@mui/material/Rating';
+import React from "react";
+import Rating from "@mui/material/Rating";
 
 // Para acessibilidade https://mui.com/material-ui/react-rating/
 
+export default function SpecificProdut({ id }) {
+  const [mainImage, setMainImage] = React.useState(0);
 
-export default function SpecificProdut({id}){
-    const [mainImage, setMainImage] = React.useState(0)
+  return (
+    <div className={styles.productPage}>
+      <div className={styles.path}>
+        home {">"} produtos em promoção {">"} <b>{product_list[0].name}</b>
+      </div>
+      <div className={styles.product}>
+        <div className={styles.productImages}>
+          {product_list[0]?.productImages.map((img, iid) => (
+            <Image
+              key={iid}
+              src={img}
+              alt={img.alt}
+              width="74"
+              height="74"
+              className={styles.image}
+              onClick={(_) => setMainImage(iid)}
+            />
+          ))}
+        </div>
 
-    return(
-       <div className={styles.productPage}>
-            <div className={styles.path}>
-                home {'>'} produtos em promoção {'>'} <b>{product_list[0].name}</b>
-            </div>
-            <div className={styles.product}>
-                <div className={styles.productImages}>
-                    {product_list[0]?.productImages.map((img, iid) => (
-                        <Image
-                        src={img}
-                        width="74"
-                        height="74"
-                        className={styles.image}
-                        onClick={(_) => setMainImage(iid)}
-                        />
-                    ))}
-                </div>
+        <div className={styles.mainImage}>
+          <Image
+            src={product_list[0].productImages[mainImage]}
+            alt={product_list[0].productImages[mainImage].alt}
+            width="499"
+            height="450"
+            className={styles.image}
+          />
+        </div>
 
-                <div className={styles.mainImage}>
-                    <Image
-                        src={product_list[0].productImages[mainImage]}
-                        width="499"
-                        height="450"
-                        className={styles.image}
-                    />
-                </div>
+        <div className={styles.productInfo}>
+          <h1>{product_list[0].name}</h1>
+          <hr />
+          <div className={styles.rating}>
+            <Rating
+              value={product_list[0].rating}
+              precision={0.5}
+              size="large"
+              getLabelText={(value) => value + "estrelas"}
+              style={{ color: "#582d18" }}
+              readOnly
+            />
+            {product_list[0].avaliationNumbers} avaliações
+          </div>
+          Cuidados:
+          <ul>
+            {product_list[0].instructions.map((el) => (
+              <li key={el}>{el}</li>
+            ))}
+          </ul>
+          Quantidade:
+          <input
+            type="number"
+            id="quantity"
+            min="0"
+            max={product_list[0].inStock}
+          />
+        </div>
 
-                <div className={styles.productInfo}>
-                    <h1>{product_list[0].name}</h1>
-                    <hr/>
-                    <div className={styles.rating}>
+        <div className={styles.buyInfo}>
+          <div className={styles.price}>
+            {product_list[0].isPromo && <h2>{product_list[0].prevPrice}</h2>}
+            <h1>{product_list[0].price}</h1>
+          </div>
 
-                        <Rating 
-                        value={product_list[0].rating}
-                        precision={0.5}
-                        size="large" 
-                        getLabelText={value => value + "estrelas"}
-                        style={{color: "#582d18"}}
-                        readOnly />
-                        {product_list[0].avaliationNumbers} avaliações
-                    </div>
-                    Cuidados:
-                    <ul>
-                        {product_list[0].instructions.map(el => (
-                            <li>{el}</li>
-                        ))}
-                    </ul>
-                    Quantidade:
-                    <input
-                    type="number"
-                    id="quantity"
-                    min="0"
-                    max={product_list[0].inStock}
-                    />
-                    
-                </div>
-
-                <div className={styles.buyInfo}>
-                    <div className={styles.price}>
-                        {product_list[0].isPromo &&
-                            <h2>{product_list[0].prevPrice}</h2>
-                        }
-                        <h1>{product_list[0].price}</h1>
-                    </div>
-
-                    <div className={styles.buyButtons}>
-                        <input
-                        type="button"
-                        value="comprar agora"
-                        className={styles.buyNow}
-                        />
-                        <input
-                        type="button"
-                        value="adicionar ao carrinho"
-                        className={styles.addToCart}
-                        />
-                    </div>
-                    
-                </div>
-
-                
-            </div>
-       </div>
-    )
+          <div className={styles.buyButtons}>
+            <input
+              type="button"
+              value="comprar agora"
+              className={styles.buyNow}
+            />
+            <input
+              type="button"
+              value="adicionar ao carrinho"
+              className={styles.addToCart}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
