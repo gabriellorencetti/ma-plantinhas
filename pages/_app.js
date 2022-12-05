b89874f57a7c0d3d59eea2f75ccd4f5ddda96c42
João Antônio Misson Milhorim <joao.misson@gmail.com>
2022-12-05 19:37:00 -0300

    adicionando vlibras
    nao ta funcionando na maioria das vezes

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Topbar from "../components/Topbar";
 import "../styles/globals.css";
 import Footer from "../components/Footer";
+import VLibras from 'vlibras-nextjs';
+
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -9,8 +11,12 @@ function MyApp({ Component, pageProps }) {
       <Topbar />
       <Component {...pageProps} />
       <Footer />
+      <div className="MyApp">
+        <VLibras forceOnload />
+      </div>
     </>
   );
 }
+
 
 export default MyApp;
