069577c1b35d771010dc459e411288f881cb9014
Maria Fernanda Mello <mafe-mello1@hotmail.com>
2022-12-05 19:37:57 -0300

    products added

--- pages/api/product_list.js
+++ pages/api/product_list.js
@@ -23,6 +23,106 @@ const product_list = [
         ],
         inStock: 10
     },
+
+    {
+        src: "/images/products/3.jpg",
+        alt:"",
+        name: "Suculenta",
+        price: "R$ 14.99",
+        isPromo: true,
+        prevPrice: "R$ 24.99",
+    },
+
+    {
+        src: "/images/products/4.jpg",
+        alt:"",
+        name: "Brinco de Princesa",
+        price: "R$ 79.99",
+        isPromo: false,
+        prevPrice: "R$ 79.99",
+    },
+
+    {
+        src: "/images/products/5.jpg",
+        alt:"",
+        name: "Bonsai Pinheiro Vermelho",
+        price: "R$ 179.99",
+        isPromo: false,
+        prevPrice: "R$ 179.99",
+    },
+
+    {
+        src: "/images/products/7.webp",
+        alt:"",
+        name: "Regador Verde 3L",
+        price: "R$ 79.99",
+        isPromo: false,
+        prevPrice: "R$ 79.99",
+    },
+
+    {
+        src: "/images/products/8.jpg",
+        alt:"",
+        name: "Kit Jardinagem Tramontina",
+        price: "R$ 108.10",
+        isPromo: true,
+        prevPrice: "R$ 120.50",
+    },
+
+    {
+        src: "/images/products/9.jfif",
+        alt:"",
+        name: "Bokashi Premium Adubo Orgânico",
+        price: "R$ 149.99",
+        isPromo: false,
+        prevPrice: "R$ 149.99",
+    },
+
+    {
+        src: "/images/products/11.webp",
+        alt:"",
+        name: "Lírio da Paz",
+        price: "R$ 36.50",
+        isPromo: false,
+        prevPrice: "R$ 36.50",
+    },
+
+    {
+        src: "/images/carousel/3.jpg",
+        alt:"",
+        name: "Trevo de Quatro Folhas",
+        price: "R$ 16.50",
+        isPromo: true,
+        prevPrice: "R$ 26.50",
+    },
+
+    {
+        src: "/images/carousel/5.webp",
+        alt:"",
+        name: "Vaso de Tulipas",
+        price: "R$ 146.50",
+        isPromo: false,
+        prevPrice: "R$ 146.50",
+    },
+
+    {
+        src: "/images/carousel/5.jpg",
+        alt:"",
+        name: "Cachepot Macramé",
+        price: "R$ 46.50",
+        isPromo: false,
+        prevPrice: "R$ 46.50",
+    },
+
+    {
+        src: "/images/carousel/6.jpg",
+        alt:"",
+        name: "Tesoura de Poda Elétrica Profissional",
+        price: "R$ 399.99",
+        isPromo: true,
+        prevPrice: "R$ 449.99",
+    },
+
 ]
 
 export default product_list
