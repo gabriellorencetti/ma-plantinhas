a76136c8a3428396546797cfa8d13402269ddea8
gabriellorencetti <lorencetti.gabriel@gmail.com>
2022-12-07 22:51:12 -0300

    Adicionando o alto contraste e aumento de fonte

--- components/Topbar.js
+++ components/Topbar.js
@@ -6,25 +6,102 @@ import CartIcon from "../icons/CartIcon";
 import UserIcon from "../icons/UserIcon";
 import Link from "next/link";
 import Dropdown from "react-bootstrap/Dropdown";
-import { useState } from "react";
+
+import { useEffect, useState } from "react";
+
+const contrastStrgKey = "isAtHighContrast";
+const cssContrastClass = "contrast";
+const resizeStrgKey = "isTextResized";
+const cssResizeClass = "larger-text";
 
 function Topbar() {
-  const [checked, setChecked] = useState();
+  const [checked, setChecked] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
+
+  // Set high contrast functions in the page
+  useEffect(() => {
+    (function () {
+      const Contrast = {
+        isAtHC: false,
+        toggle: function () {
+          this.setContrast(!this.isAtHC);
+        },
+        setContrast: function (isAtHighContrast) {
+          localStorage[contrastStrgKey] = "" + isAtHighContrast;
+          this.isAtHC = isAtHighContrast;
+          this.updateSiteBody();
+        },
+        updateSiteBody: function () {
+          const body = document.body;
+          if (!body) return;
+
+          this.isAtHC = this.isAtHC || localStorage[contrastStrgKey] === "true";
+
+          if (this.isAtHC) {
+            body.classList.add(cssContrastClass);
+          } else {
+            body.classList.remove(cssContrastClass);
+          }
+        },
+      };
+
+      window.toggleContrast = function () {
+        Contrast.toggle();
+      };
+
+      Contrast.updateSiteBody();
+    })();
+
+    // Set text resizing functions in the page
+    (function () {
+      const TextResize = {
+        isResized: false,
+        toggle: function () {
+          this.setResize(!this.isResized);
+        },
+        setResize: function (isResized) {
+          localStorage[resizeStrgKey] = "" + isResized;
+          this.isResized = isResized;
+          this.updateSiteBody();
+        },
+        updateSiteBody: function () {
+          const body = document.body;
+          if (!body) return;
+
+          this.isResized =
+            this.isResized || localStorage[resizeStrgKey] === "true";
+
+          if (this.isResized) {
+            body.classList.add(cssResizeClass);
+          } else {
+            body.classList.remove(cssResizeClass);
+          }
+        },
+      };
+
+      window.toggleResize = function () {
+        TextResize.toggle();
+      };
+
+      TextResize.updateSiteBody();
+    })();
+  });
 
   return (
     <Navbar variant="dark" expand="lg" className={styles.navbar}>
       <Container>
-        <Navbar.Brand href="/" className={styles.navbarBrand}>
-          <Image
-            alt=""
-            src="/images/logo.svg"
-            width="48"
-            height="48"
-            className="d-inline-block align-top"
-          />{" "}
-          MaPlantinhas
-        </Navbar.Brand>
+        <Link href="/">
+          <Navbar.Brand className={styles.navbarBrand}>
+            <Image
+              alt=""
+              src="/images/logo.svg"
+              width="48"
+              height="48"
+              className="d-inline-block align-top"
+            />{" "}
+            MaPlantinhas
+          </Navbar.Brand>
+        </Link>
 
         <div className={styles.navbarLeftItems}>
           <Dropdown className={styles.dropdownColor} autoClose="outside">
@@ -33,52 +110,60 @@ function Topbar() {
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
-              <Dropdown.Item onClick={() => setChecked(!checked)}>
+              <Dropdown.Item
+                onClick={() => {
+                  window.toggleContrast();
+                }}
+                onKeyDown={() => {
+                  window.toggleContrast();
+                }}
+                accessKey="1"
+              >
                 <div className={styles.row}>
                   <label className={styles.switch}>
-                    <input type="checkbox" checked={checked}></input>
+                    <input
+                      type="checkbox"
+                      checked={checked}
+                      onChange={() => setChecked(!checked)}
+                    ></input>
                     <span className={styles.slider}></span>
                   </label>
-                  Auto-Contraste
+                  Alto contraste
                 </div>
               </Dropdown.Item>
-
-              <Dropdown.Item onCLick={() => {}}>Fonte Pequena</Dropdown.Item>
-
               <Dropdown.Item
-                onCLick={() => {
-                  document.body.style.fontSize = 1.2;
+                onClick={() => {
+                  window.toggleResize();
                 }}
-              >
-                Fonte Média
-              </Dropdown.Item>
-
-              <Dropdown.Item
-                onCLick={() => {
-                  document.body.style.fontSize = 1.4;
+                onKeyDown={() => {
+                  window.toggleResize();
                 }}
+                accessKey="2"
               >
-                Fonte Grande
+                Aumentar/diminuir texto
               </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
-
           <Navbar.Text>
-            {loggedIn ?
+            {loggedIn ? (
               <input
-              type="button"
-              value="Bem vindo, Karen"
-              className={styles.loginButton}
-              onClick={(event) => setLoggedIn(lastLoginState => !lastLoginState)}
+                type="button"
+                value="Boas vindas, Karen!"
+                className={styles.loginButton}
+                onClick={(event) =>
+                  setLoggedIn((lastLoginState) => !lastLoginState)
+                }
               />
-              :
+            ) : (
               <input
-              type="button"
-              value="Entrar"
-              className={styles.loginButton}
-              onClick={(event) => setLoggedIn(lastLoginState => !lastLoginState)}
+                type="button"
+                value="Entrar"
+                className={styles.loginButton}
+                onClick={(event) =>
+                  setLoggedIn((lastLoginState) => !lastLoginState)
+                }
               />
-            }
+            )}
           </Navbar.Text>
           <Navbar.Text>
             <Link href="cart">
